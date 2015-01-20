34db4259389b4f77b5811032046fc32ff5df85bd
chh <195822080@qq.com>
2015-01-20 18:32:06 +0800

    'first_commit'

---
+++ src/main/java/com/controller/GupiaoController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2015-01-20.
+ */
+@Controller
+public class GupiaoController {
+
+    @RequestMapping("chen")
+    public void chen() {
+        System.out.println("你好");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2015-01-20
+  Time: 15:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>你好吗</title>
+</head>
+<body>
+陈鸿浩
+</body>
+</html>
