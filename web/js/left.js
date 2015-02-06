048fb5bbc8c88901922eee1897f4a7ca79a4e720
hao <spieled916@gmail.com>
2015-02-06 09:26:56 +0800

    添加查询ip访问的页面

--- web/js/left.js
+++ web/js/left.js
@@ -3,11 +3,11 @@ $(function () {
         addTabs('index_tab', '股票', '/view/market.html');
     });
 
-    $('#address').bind('click', function () {
-        addTabs('index_tab', '地址', '/view/address.html');
-    });
-
     $('#macd').bind('click', function () {
         addTabs('index_tab', '金叉', '/view/macd.html');
+    });
+
+    $('#address').bind('click', function () {
+        addTabs('index_tab', '地址', '/view/address.html');
     });
 });
